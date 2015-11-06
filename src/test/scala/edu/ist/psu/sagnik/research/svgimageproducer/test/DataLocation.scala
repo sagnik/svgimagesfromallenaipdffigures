258fb88c8bc0b86293a032bd788a925362fa37ea
sagnik <sagnik1987@gmail.com>
2015-11-06 17:35:50 -0500

    added a test file where pdf to svg conversion with petermr code doesn't work well. Need more testing on this

--- src/test/scala/edu/ist/psu/sagnik/research/svgimageproducer/test/DataLocation.scala
+++ src/test/scala/edu/ist/psu/sagnik/research/svgimageproducer/test/DataLocation.scala
@@ -6,7 +6,9 @@ package edu.ist.psu.sagnik.research.svgimageproducer.test
 object DataLocation {
   //val pdfLoc="src/test/resources/10.1.1.353.3090.pdf"
   //val figureJSONLoc="src/test/resources/10.1.1.353.3090-Figure-5-mod.json"
-  val pdfLoc="src/test/resources/10.1.1.101.912.pdf"
-  val figureJSONLoc="src/test/resources/10.1.1.101.912-Figure-2-mod.json"
+  //val pdfLoc="src/test/resources/10.1.1.101.912.pdf"
+  //val figureJSONLoc="src/test/resources/10.1.1.101.912-Figure-2-mod.json"
+  val pdfLoc="src/test/resources/10.1.1.231.1422.pdf"
+  val figureJSONLoc="src/test/resources/10.1.1.231.1422-Figure-2-mod.json"
 
 }
